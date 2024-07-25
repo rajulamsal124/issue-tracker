6cc87ca6e654baeae06d5fc0ab17178704573947
Raju Lamsal <rajusoft@Rajus-Laptop.local>
2024-07-25 22:35:52 +1000

    handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
 
 // Zod validation
-const createIssueSchema = z.object({ title: z.string().min(1).max(255) });
+const createIssueSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required").max(255),
+});
 export async function POST(request: NextRequest) {
   const prisma = new PrismaClient();
   const body = await request.json();
